eeefc2e23cc692b2cc435d9ff091f6cf1adfdde1
Antoine WEBER <a.weber@sdv.fr>
2015-09-15 10:40:12 +0200

    ajout page de channel

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var file = require('./routes/file');
+var channel = require('./routes/channel');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/file', file);
+app.use('/channel', channel);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -7,25 +7,7 @@ router.get('/', function(req, res, next) {
   var config = require('../config');
   var db = config.init_db();
 
-  var html = "<!doctype html><html lang='fr'><head><meta charset='UTF-8'><head>"
-  +"<script type='text/javascript' src='//code.jquery.com/jquery-1.11.3.min.js'></script>"
-  +"<script type='text/javascript' src='https://cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js'></script>"
-  +"<script type='text/javascript' src='https://cdn.datatables.net/1.10.9/js/dataTables.bootstrap.min.js'></script>"
-  +"<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>"
-  +"<link rel='stylesheet' type='text/css' href='https://cdn.datatables.net/1.10.9/css/dataTables.bootstrap.min.css'>"
-
-
-  +"</head><body><h1>liste des fichiers</h1>"
-  + '<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">'
-  + '      <thead>'
-  + '          <tr>'
-  + '              <th>Nom</th>'
-  + '              <th>Durée</th>'
-  + '              <th>Année</th>'
-  + '          </tr>'
-  + '      </thead>'
-  + '      <tbody>';
-
+  var data = [];
   //on fais toute les opération de base a la suite
   db.serialize(function() {
 
@@ -36,28 +18,18 @@ router.get('/', function(req, res, next) {
     //}
     //stmt.finalize();
 
-    db.each("SELECT i.id as id, i.title as title, p.file as file, i.duration as second, i.year as year"
-    + " FROM metadata_items i, media_parts p "
-    + " WHERE p.media_item_id=i.id ORDER BY i.title ASC", function(err, row) {
-        var tab = row.file.split('/');
-        var tab2 = tab[tab.length -1].split('\\');
-        var filename = tab2[tab2.length -1];
-        if(row.title != '')
-        html += "<tr><td><a href='/file/"+ row.id + "/"+filename+"'> " + row.title + "</a></td><td>"+row.second+"</td><td>"+row.year+"</td></tr>";
+    db.each("SELECT id, name"
+    + " FROM library_sections ORDER BY name ASC", function(err, row) {
+      data.push(row);
     },
     //aprés toute les opération de la base
     function() {
-        // All done fetching records, render response
-        html += "</tbody></table><script>$(document).ready(function() {"
-    + "$('#example').DataTable();"
-    + "} );</script></body></html>";
-        res.end(html);
+        res.render('index', { title: 'Liste des channel',channels: data });
     });
 
   });
 
   db.close();
-  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
