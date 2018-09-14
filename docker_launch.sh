f58185a4332b64ec6323c06b0c431e6821b939cd
root <root@localhost.localdomain>
2018-09-14 17:41:02 +0200

    work on node 6 for docker

--- docker_launch.sh
+++ docker_launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker run  --rm --name plex-wd -v "$PWD":/usr/src/app -v "/var/lib/plexmediaserver":"/var/lib/plexmediaserver" -w /usr/src/app -it node:latest bash -c "npm install && npm start"
+source .env
+docker run  --rm --name plex-wd -p "${HTTP_PORT}:3000" -v "$PWD":/usr/src/app -v "/var/lib/plexmediaserver":"/var/lib/plexmediaserver" -w /usr/src/app -it node:6 bash -c "npm install && npm start"
 
